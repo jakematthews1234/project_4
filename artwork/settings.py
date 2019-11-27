de8312a1ae2eb5585a03bb221bc9fc9083552fb5
jakematthews1234 <jakematthews072@gmail.com>
2019-11-27 21:03:33 +0000

    moved dotenv loading to top of settings.py file, added procfile

--- artwork/settings.py
+++ artwork/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 
+if os.path.exists(".env"):
+    from dotenv import load_dotenv
+    load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -132,7 +136,3 @@ STATICFILES_DIRS = (
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
-
-if os.path.exists(".env"):
-    from dotenv import load_dotenv
-    load_dotenv()
