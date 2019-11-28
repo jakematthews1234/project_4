c26cffc8521ad7d588ca2751a3c1e31ce63bdb8e
jakematthews1234 <jakematthews072@gmail.com>
2019-11-28 16:53:12 +0000

    changed settings to try set up heroku

--- artwork/settings.py
+++ artwork/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jake-matthews-project-4.herokuapp.com',
+                 ]
 
 
 # Application definition
