8d63e0862e06cb3b2dbe641c8dd5b5184822f551
jakematthews1234 <jakematthews072@gmail.com>
2019-11-28 16:29:46 +0000

    add commenting to base.html and moved blogs from being seen when signed out to only when signed in

--- artwork/settings.py
+++ artwork/settings.py
@@ -132,9 +132,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
-
+# Stripe env keys
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
